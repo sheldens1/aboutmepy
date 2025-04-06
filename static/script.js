c636c0d10e3117c10ec305ed7aed5c35736c91f6
sheldens1 <159596263+sheldens1@users.noreply.github.com>
2025-04-06 11:38:31 +0700

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggleSettings");
+    const panel = document.getElementById("settingsPanel");
+    let isOpen = false;
+    let sudahDitutup = false;
+
+    toggleBtn.addEventListener("click", function () {
+        if (sudahDitutup) return;
+
+        if (!isOpen) {
+            panel.classList.add("active");
+            isOpen = true;
+        } else {
+            panel.classList.remove("active");
+            sudahDitutup = true; // habis nutup ga bisa dibuka lagi
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggleSettings");
+    const panel = document.getElementById("settingsPanel");
+
+    toggleBtn.addEventListener("click", function () {
+        panel.classList.toggle("active");
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggleSettings");
+    const settingsPanel = document.getElementById("settingsPanel");
+
+    toggleBtn.addEventListener("click", function () {
+        settingsPanel.classList.toggle("active");
+    });
+});
